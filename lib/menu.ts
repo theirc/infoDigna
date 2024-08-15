d0d5f89d5347caa0c7fec1be56fa8a2f37d8f688
Pascalsala <146017634+Pascalsala@users.noreply.github.com>
2024-08-15 13:11:56 +0300

    Update menu.ts

--- lib/menu.ts
+++ lib/menu.ts
@@ -27,8 +27,8 @@ export function getFooterItems(
   for (const { category } of categories as CategoryWithSections[]) {
     if (category.id === 6592468755101) {
       items.push({
-        key: category.id.toString(),
-        label: category.name,
+        key: 'services',
+        label: strings.services,
         href: '/#service-map',
       });
     }
